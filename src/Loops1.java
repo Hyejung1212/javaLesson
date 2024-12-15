df0e23a655d7867cf0c1d9a6e860d432af3f9f17
ihyezzi <ihyezzi@gmail.com>
2024-12-15 15:05:03 +0900

    20241215 commit

--- src/Loops1.java
+++ src/Loops1.java
@@ -1,8 +1,46 @@
 public class Loops1 {
     public static void main(String[] args) {
-        /*
-        * while 반복문
-        *
-        * */
+        // 반복문
+        // while ( 조건식 )
+        // {
+        //    위 조건식이 참(true)일때 실행되는 명령문들....
+        // }
+
+        int i = 0;
+        while ( i < 5 ) {
+            System.out.println("i의 값 : " + i);
+                // i = i + 1;
+                 i += 1;
+
+        }
+        System.out.println("while 반복문 종료");
+
+        System.out.println();
+        i = 0;
+        while ( i < 10 ) {
+            System.out.println("i의 값 : " + i);
+            // i = i + 1;
+            i += 2;
+        }
+
+        i = 1;
+        int total = 0; // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+        while ( i <= 10 ) {
+            total += i;
+            i ++;
+        }
+        System.out.println( "total + " + total );
+
+            //  total = 1 + 3 + 5 + 7 + 9
+
+        i = 1;
+        total = 0;
+        while ( i < 10 ) {
+            total += i;
+            i += 2;
+
+        }
+        System.out.println( "total + " + total );
     }
+
 }
